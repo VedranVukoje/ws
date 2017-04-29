5ce490c1e056c35ed144eb582ae120d055048293
Vedran Vukoje <bolitekuracudupe@gmail.com>
2017-04-29 18:19:05 +0200

    nis spec

--- tests/Server/server1.php
+++ tests/Server/server1.php
@@ -10,37 +10,44 @@ namespace {
 }
 
 namespace Wsa\Ws {
-    
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\HttpFoundation\Request;
     use Zend\Soap\AutoDiscover;
 
     $request = Request::createFromGlobals();
-    
+
+    class Server1
+    {
+        public function remoteTest()
+        {
+            return 'remoteTest';
+        }
+    }
+
 //    echo $request->getMethod();
     if ('GET' == $request->getMethod()) {
-        
+
         $autoDiscover = new AutoDiscover();
         $autoDiscover->setUri('http://learn.symfony/wsaws.php');
         $autoDiscover->setServiceName('WsaWs');
-        $autoDiscover->setClass(Tsi::class);
+        $autoDiscover->setClass(Server1::class);
         $wsdl = $autoDiscover->generate();
-        
+
         $response = (new Response())->setContent($wsdl->toXML());
-        
-        $response->headers->set('Content-Type','text/xml; charset=utf-8');
-        
+
+        $response->headers->set('Content-Type', 'text/xml; charset=utf-8');
+
         $response->send();
     }
-    
-    if('POST' == $request->getMethod()){
-        $server = new \Zend\Soap\Server("http://learn.symfony/wsaws.php",[
+
+    if ('POST' == $request->getMethod()) {
+        $server = new \Zend\Soap\Server("http://learn.symfony/wsaws.php", [
             'actor' => "http://learn.symfony/wsaws.php"
         ]);
-        $server->setClass(Tsi::class);
+        $server->setClass(Server1::class);
         $server->handle();
     }
-
 }
 
 namespace {
